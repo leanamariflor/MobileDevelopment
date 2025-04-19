a6ff5455fc373ccad63bbe19336fb9ca46e63649
jonvicbarcenas <exchantpro@gmail.com>
2025-04-19 16:28:27 +0800

    refactor: remove unused character drawable resources from MovieData

--- app/src/main/java/com/anime/aniwatch/data/MovieData.kt
+++ app/src/main/java/com/anime/aniwatch/data/MovieData.kt
@@ -25,22 +25,5 @@ object MovieData {
         R.drawable.zoro,
         R.drawable.luffy,
         R.drawable.sanji,
-        R.drawable.robin,
-        R.drawable.nami,
-        R.drawable.yamato,
-        R.drawable.chopper,
-        R.drawable.ussop,
-        R.drawable.brook,
-        R.drawable.jimbie,
-        R.drawable.franky,
-        R.drawable.law
-
-
-
-
-
-
-
-
         )
 }
